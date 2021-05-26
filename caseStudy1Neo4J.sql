ced0980df04676064dcb9f293617616eb9659b8d
ericeugenio <72433149+ericeugenio@users.noreply.github.com>
2021-05-26 12:09:17 +0200

    Update caseStudy1Neo4J.sql

--- caseStudy1Neo4J.sql
+++ caseStudy1Neo4J.sql
@@ -24,7 +24,7 @@ MERGE (ci:City {cityID: (toInteger(csv.CityID)),
     name: csv.CityName,
     timezone: toInteger(csv.Timezone)})
 MERGE (a)-[l:LOCATION]->(ci)
-MERGE (ci)-[i:INCOUNTRY]->(c)
+MERGE (ci)-[i:IN_COUNTRY]->(c)
 
 
 LOAD CSV WITH HEADERS FROM
